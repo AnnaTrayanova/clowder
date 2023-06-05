de4510def01ba14d3e98cf9181abebfcca20eb66
Svet <125960896+akiiyamas@users.noreply.github.com>
2023-06-05 11:29:16 +0000

    Pending changes exported from your codespace

--- snake.h
+++ snake.h
@@ -1,5 +1,39 @@
-#include <iostream>
-#include <string>
+#ifndef SNAKE_H
+#define SNAKE_H
 
-int catTailX[100];//100 mn li e?
-int catTailY[100];
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+class SnakeGame
+{
+public:
+    SnakeGame();
+    ~SnakeGame();
+    void Run();
+private:
+    // game settings
+    const int width;
+    const int height;
+    int x, y, fruitX, fruitY, score;
+    int tailX[100], tailY[100];
+    int nTail;
+    enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+    eDirection dir;
+
+    // image display variables
+    const string imageName;
+    HANDLE hImage;
+    int imageWidth, imageHeight;
+
+    // function prototypes
+    void Draw();
+    void Setup();
+    void Input();
+    void Logic();
+    void ShowImage();
+};
+
+#endif
