84faf2d47955721ebae342e1862f15b933632923
Sss <125960911+SimonaNi@users.noreply.github.com>
2023-06-11 00:35:23 +0300

    note

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 #include <conio.h>
 #include "snake.h"
 #include "window.h"
-//no graphics idk how work help
+/* I don't know what else to do, I tried so many things
+Theres something wrong with the Qt, not al the code is
+finished because I wanted to fix the errors first but I
+couldn't. There's even some things that repeat*/
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
